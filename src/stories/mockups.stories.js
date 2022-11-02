4a1305041667a905b40684c13d2314d18d3f5445
ben-rinehart <brinehart@csgactuarial.com>
2022-11-02 11:28:56 -0500

    initial commit

---
+++ src/stories/mockups.stories.js
@@ -0,0 +1,129 @@
+import React from "react";
+import Button from "../components/Button";
+import Icon from "../components/Icon";
+import Formgroup from "../components/Formgroup";
+
+export default {
+  title: "Mockups",
+}
+
+export const EAPP4915 = (args) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md">
+        <Formgroup
+          formElement="input"
+          inputID="textfieldID"
+          inputLabel="Drug Name"
+          textfieldType="text"
+          isInputRequired
+        />
+      </div>
+      <div className="col-md">
+        <Formgroup
+          formElement="select"
+          inputID="dosage"
+          inputLabel="Dosage"
+          inputDisabled={true}
+          isInputRequired
+          selectOptions={[
+            {
+              label: '',
+            },
+            {
+              label: 'First Option',
+              value: 'a'
+            },
+            {
+              label: 'Second Option',
+              value: 'b'
+            },
+            {
+              label: 'Third Option',
+              value: 'b'
+            }
+          ]}
+        />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-md">
+        <Formgroup
+          formElement="input"
+          inputID="diagnosis"
+          inputLabel="Diagnosis"
+          textfieldType="text"
+          isInputRequired
+        />
+      </div>
+      <div className="col-md">
+        <Formgroup
+          formElement="input"
+          inputID="medstartDate"
+          inputLabel="Medication Start Date"
+          textfieldType="date"
+        />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-md">
+        <Formgroup
+          formElement="input"
+          inputID="numberID"
+          inputLabel="Quantity Taken Each Time"
+          textfieldType="number"
+          textfieldMin={1}
+          textfieldMax={10}
+          isInputRequired
+        />
+      </div>
+      <div className="col-md">
+        <Formgroup
+          formElement="input"
+          inputID="numberID"
+          inputLabel="Number of Times Taken"
+          textfieldType="number"
+          textfieldMin={1}
+          textfieldMax={10}
+          isInputRequired
+        />
+      </div>
+      <div className="col-md-6">
+        <Formgroup
+          formElement="select"
+          inputID="frequency"
+          inputLabel="Frequency"
+          isInputRequired
+          selectOptions={[
+            {
+              label: 'Daily',
+              value: 'a'
+            },
+            {
+              label: 'Second Option',
+              value: 'b'
+            },
+            {
+              label: 'Third Option',
+              value: 'b'
+            }
+          ]}
+        />
+      </div>
+    </div>
+    <div className="text-right mb-5">
+      <Button
+        btnStyle="btn-secondary"
+      >
+        <Icon
+          iconStyle="fas"
+          iconName="fa-plus"
+        />
+        Add Rx Drug
+      </Button>
+    </div>
+  </div>
+);
+EAPP4915.args = {
+  badgeType: "badge-primary",
+};
---
+++ src/stories/sandbox.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../components/Button";
+import Icon from "../components/Icon";
+import Formgroup from "../components/Formgroup";
+
+export default {
+  title: "Sandbox",
+}
+
+export const Testing = (args) => (
+  <div className="container">
+
+  </div>
+);
+Testing.args = {
+};
