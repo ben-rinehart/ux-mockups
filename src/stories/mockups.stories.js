7d63f03c8ae576832d34b17018138eeeb39943e5
ben-rinehart <brinehart@csgactuarial.com>
2022-11-22 09:08:29 -0600

    Filter Modal mockup

--- src/stories/mockups.stories.js
+++ src/stories/mockups.stories.js
@@ -3,6 +3,11 @@ import Formgroup from "../components/Formgroup";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
 
+import List from "../components/List";
+import ListItem from "../components/ListItem";
+import Avatar from "../components/Avatar";
+import Modal from "../components/Modal";
+
 export default {
   title: "Mockups",
 }
@@ -18,6 +23,7 @@ export const EAPP4915 = (args) => (
           inputLabel="Drug Name"
           textfieldType="text"
           isInputRequired
+          inputTabIndex="1"
         />
       </div>
       <div className="col-md">
@@ -25,8 +31,9 @@ export const EAPP4915 = (args) => (
           formElement="select"
           inputID="dosage"
           inputLabel="Dosage"
-          inputDisabled={true}
+          // inputDisabled={true}
           isInputRequired
+          inputTabIndex="2"
           selectOptions={[
             {
               label: '',
@@ -55,6 +62,7 @@ export const EAPP4915 = (args) => (
           inputLabel="Diagnosis"
           textfieldType="text"
           isInputRequired
+          inputTabIndex="3"
         />
       </div>
       <div className="col-md">
@@ -63,6 +71,7 @@ export const EAPP4915 = (args) => (
           inputID="medstartDate"
           inputLabel="Medication Start Date"
           textfieldType="date"
+          inputTabIndex="4"
         />
       </div>
     </div>
@@ -73,10 +82,11 @@ export const EAPP4915 = (args) => (
           inputID="numberID"
           inputLabel="Quantity Taken Each Time"
           textfieldType="number"
-          inputValue="1"
           textfieldMin="1"
           textfieldMax="10"
           isInputRequired
+          inputPlaceholder="1-10"
+          inputTabIndex="5"
         />
       </div>
       <div className="col-md">
@@ -85,10 +95,11 @@ export const EAPP4915 = (args) => (
           inputID="numberID"
           inputLabel="Number of Times Taken"
           textfieldType="number"
-          inputValue="1"
           textfieldMin="1"
           textfieldMax="10"
           isInputRequired
+          inputPlaceholder="1-10"
+          inputTabIndex="6"
         />
       </div>
       <div className="col-md-6">
@@ -97,6 +108,7 @@ export const EAPP4915 = (args) => (
           inputID="frequency"
           inputLabel="Frequency"
           isInputRequired
+          inputTabIndex="7"
           selectOptions={[
             {
               label: 'Daily',
@@ -125,8 +137,210 @@ export const EAPP4915 = (args) => (
         Add Rx Drug
       </Button>
     </div>
+
+    <List listID="robustList">
+    <ListItem>
+      <div className="d-flex align-items-center">
+        <div className="mr-3">
+          <Avatar
+            avatarBgColor="bg-light"
+            avatarShape="rounded-circle"
+            avatarSize="csg-avatar-sm"
+            avatarTxtColor="text-black-50"
+          >
+            <Icon
+              iconName="fa-pills"
+              iconStyle="fas"
+            />
+          </Avatar>
+        </div>
+        <div className="mr-auto">
+          <p className="mb-0 font-weight-bold">
+            LIALDA (Mesalamine)
+            <span className="small pl-4">10MG Tabs</span>
+          </p>
+          <p className="small text-muted mb-0">
+            Started on Sun Oct 30 2022: 2 taken 3 times daily for high cholesterol
+          </p>
+        </div>
+        <div className="ml-2 csg-actions">
+          <Button btnClass="btn-link text-danger">
+            <Icon
+              iconName="fa-trash-alt"
+              iconStyle="fas"
+            />
+          </Button>
+        </div>
+      </div>
+    </ListItem>
+    <ListItem>
+      <div className="d-flex align-items-center">
+        <div className="mr-3">
+          <Avatar
+            avatarBgColor="bg-light"
+            avatarShape="rounded-circle"
+            avatarSize="csg-avatar-sm"
+            avatarTxtColor="text-black-50"
+          >
+            <Icon
+              iconName="fa-pills"
+              iconStyle="fas"
+            />
+          </Avatar>
+        </div>
+        <div className="mr-auto">
+          <p className="mb-0 font-weight-bold">
+            LIBRAX (Chlordiazepoxide HCI-Clidinium Bromide))
+            <span className="small pl-4">80MG Tabs</span>
+          </p>
+          <p className="small text-muted mb-0">
+            Started on Sun Oct 30 2022: 1 taken twice daily for high cholesterol
+          </p>
+        </div>
+        <div className="ml-2 csg-actions">
+          <Button btnClass="btn-link text-danger">
+            <Icon
+              iconName="fa-trash-alt"
+              iconStyle="fas"
+            />
+          </Button>
+        </div>
+      </div>
+    </ListItem>
+    </List>
   </div>
 );
 EAPP4915.args = {
   badgeType: "badge-primary",
 };
+
+export const CSG5685 = (args) => (
+  <div className="container">
+      <Modal {...args}>
+        <div className="modal-header">
+          <h5 className="modal-title">
+            Filters
+          </h5>
+          <Button btnClass="close">
+            <span>×</span>
+          </Button>
+        </div>
+        <div className="modal-body">
+          <div className="row">
+            <div className="col">
+              <Formgroup
+                formElement="multiselect"
+                inputID="subsidy"
+                inputLabel="Low Income Subsidy"
+                isMulti
+                onClick={function noRefCheck(){}}
+                selectOptions={[
+                  {
+                    label: 'None',
+                    value: 'a'
+                  },
+                  {
+                    label: '100',
+                    value: 'b'
+                  },
+                  {
+                    label: '75',
+                    value: 'b'
+                  },
+                  {
+                    label: '50',
+                    value: 'b'
+                  },
+                  {
+                    label: '25',
+                    value: 'b'
+                  }
+                ]}
+              />
+            </div>
+            <div className="col">
+              <Formgroup
+                formElement="multiselect"
+                inputID="sortby"
+                inputLabel="Sort By"
+                isMulti
+                onClick={function noRefCheck(){}}
+                selectOptions={[
+                  {
+                    label: 'Monthly Premium',
+                    value: 'a'
+                  },
+                ]}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <Formgroup
+                formElement="multiselect"
+                inputID="filter"
+                inputLabel="Filters"
+                isMulti
+                onClick={function noRefCheck(){}}
+                selectOptions={[
+                  {
+                    label: 'Carrier',
+                    value: 'a'
+                  },
+                ]}
+              />
+            </div>
+            <div className="col">
+              <div className="form-row align-items-center">
+                <div className="col">
+                  <Formgroup
+                    formElement="multiselect"
+                    inputID="includedCarriers"
+                    inputLabel="Included Carriers"
+                    isMulti
+                    selectOptions={[
+                      {
+                        label: '',
+                        value: 'a'
+                      },
+                    ]}
+                  />
+                </div>
+                <div className="col-2 text-right">
+                  <Button
+                    btnStyle="btn-link"
+                    btnClass="mt-3"
+                  >
+                    <Icon iconStyle="fas" iconName="fa-trash-alt" iconColor="text-danger" />{' '}
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Button
+            btnStyle="btn-link"
+            b
+          >
+            <Icon iconStyle="fas" iconName="fa-plus" />{' '}
+            Add Filter
+          </Button>
+        </div>
+        <div className="modal-footer">
+            <Button
+              btnStyle="btn-secondary"
+            >
+              Cancel
+            </Button>
+            <Button
+              btnStyle="btn-primary"
+            >
+              Refine Results
+            </Button>
+        </div>
+      </Modal>
+  </div>
+);
+CSG5685.args = {
+  isModalOpen: true,
+  modalWidth: "modal-lg",
+}
